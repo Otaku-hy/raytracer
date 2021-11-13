905e8623ee080819283817d14c2bfeb46e61b173
张航宇 <zhanghangyu86@gmail.com>
2021-11-13 23:51:18 +0800

    full version

--- main.cpp
+++ main.cpp
@@ -1,92 +1,112 @@
-#include <iostream>
-#include <cstdlib>
-#include "lib/Eigen/Core"
-#include "Ray.hpp"
-#include "Utils.hpp"
-#include "Object.hpp"
-#include "Plane.hpp"
-#include "Sphere.hpp"
+#include "pbrt.hpp"
+#include "OBJ_Loader.hpp"
 #include "Triangle.hpp"
-#include "Scene.hpp"
-#include "Shader.hpp"
-#include "test.cpp"
+#include "GeometryPrimitive.hpp"
+#include "AreaLight.hpp"
+#include "MatteMaterial.hpp"
+#include "MitchellFilter.hpp"
+#include "PerspectiveCamera.hpp"
+#include "StratifiedSampler.hpp"
+#include "PathIntegrator.hpp"
 
-using namespace Eigen;
-
-void cast_ray(Scene scene)
+void Preprocess(std::vector<std::shared_ptr<Primitive>> &p, std::vector<std::shared_ptr<Light>> &lights)
 {
-    float aspect_ratio = scene.camera->width / float(scene.camera->height);
-    float fov = 45.0 / 360.0 * PI * 2;
+    std::shared_ptr<Loader> model1 = std::make_shared<Loader>("../Model/tallbox.obj");
+    std::shared_ptr<Loader> model2 = std::make_shared<Loader>("../Model/shortbox.obj");
+    std::shared_ptr<Loader> model3 = std::make_shared<Loader>("../Model/right.obj");
+    std::shared_ptr<Loader> model4 = std::make_shared<Loader>("../Model/left.obj");
+    std::shared_ptr<Loader> model5 = std::make_shared<Loader>("../Model/floor.obj");
+    std::shared_ptr<Loader> model6 = std::make_shared<Loader>("../Model/light.obj");
+    model1->loadFile();
+    model2->loadFile();
+    model3->loadFile();
+    model4->loadFile();
+    model5->loadFile();
+    model6->loadFile();
 
-    float height = 2 * tan(fov / 2.0);
-    float width = height * aspect_ratio;
+    std::shared_ptr<TriangleMesh> mesh1 = model1->createTriangleMesh();
+    std::shared_ptr<TriangleMesh> mesh2 = model2->createTriangleMesh();
+    std::shared_ptr<TriangleMesh> mesh3 = model3->createTriangleMesh();
+    std::shared_ptr<TriangleMesh> mesh4 = model4->createTriangleMesh();
+    std::shared_ptr<TriangleMesh> mesh5 = model5->createTriangleMesh();
+    std::shared_ptr<TriangleMesh> mesh6 = model6->createTriangleMesh();
 
-    for (int i = 0; i < scene.camera->width; i++)
+    std::shared_ptr<Matrix4_4> P = std::make_shared<Matrix4_4>();
+    std::shared_ptr<Matrix4_4> O = std::make_shared<Matrix4_4>();
+
+    for (int i = 0; i < mesh1->nTriangles; i++)
     {
-        for (int j = 0; j < scene.camera->height; j++)
-        {
-            for (int k = 0; k < scene.camera->SPP; k++)
-            {
-                float coord_x = i + 0.5;
-                float coord_y = j + 0.5;
-                coord_x = (coord_x - scene.camera->width / 2.0) * width / float(scene.camera->width);
-                coord_y = (coord_y - scene.camera->height / 2.0) * height / float(scene.camera->height);
-
-                Vector3f rayDir = Vector3f(coord_x, coord_y, -1.0).normalized();
-                rayDir = scene.camera->translate * rayDir;
-                Ray ray(Vector3f(0, 0, 0), rayDir);
-
-                float interpolar = j / float(scene.camera->height);
-                Vector3f start(1.0, 1.0, 1.0);
-                Vector3f end(0.5, 0.7, 1.0);
-
-                Vector3f background = start + interpolar * (end - start);
-
-                Intersection intersection;
-
-                if (scene.scene_intersection(ray, intersection))
-                {
-                    screen[i][j] += rendering(scene, intersection, 0);
-                }
-                else
-                {
-                    screen[i][j] += background;
-                }
-            }
-
-            screen[i][j] /= float(scene.camera->SPP);
-        }
+        std::shared_ptr<Triangle> tri(new Triangle(mesh1, i, P, O));
+        std::shared_ptr<Material> m = std::make_shared<MatteMaterial>(Vector3f(1, 1, 1), 20);
+        std::shared_ptr<AreaLight> l = NULL;
+        std::shared_ptr<GeometryPrimitive> gP(new GeometryPrimitive(tri, m, l));
+        p.push_back(gP);
     }
+    for (int i = 0; i < mesh2->nTriangles; i++)
+    {
+        std::shared_ptr<Triangle> tri(new Triangle(mesh2, i, P, O));
+        std::shared_ptr<Material> m = std::make_shared<MatteMaterial>(Vector3f(1.0, 1.0, 1.0), 20);
+        std::shared_ptr<AreaLight> l = NULL;
+        std::shared_ptr<GeometryPrimitive> gP(new GeometryPrimitive(tri, m, l));
+        p.push_back(gP);
+    }
+    for (int i = 0; i < mesh3->nTriangles; i++)
+    {
+        std::shared_ptr<Triangle> tri(new Triangle(mesh3, i, P, O));
+        std::shared_ptr<Material> m = std::make_shared<MatteMaterial>(Vector3f(0, 1, 0), 20);
+        std::shared_ptr<AreaLight> l = NULL;
+        std::shared_ptr<GeometryPrimitive> gP(new GeometryPrimitive(tri, m, l));
+        p.push_back(gP);
+    }
+    for (int i = 0; i < mesh4->nTriangles; i++)
+    {
+        std::shared_ptr<Triangle> tri(new Triangle(mesh4, i, P, O));
+        std::shared_ptr<Material> m = std::make_shared<MatteMaterial>(Vector3f(1, 0, 0), 20);
+        std::shared_ptr<AreaLight> l = NULL;
+        std::shared_ptr<GeometryPrimitive> gP(new GeometryPrimitive(tri, m, l));
+        p.push_back(gP);
+    }
+    for (int i = 0; i < mesh5->nTriangles; i++)
+    {
+        std::shared_ptr<Triangle> tri(new Triangle(mesh5, i, P, O));
+        std::shared_ptr<Material> m = std::make_shared<MatteMaterial>(Vector3f(1, 1, 1), 20);
+        std::shared_ptr<AreaLight> l = NULL;
+        std::shared_ptr<GeometryPrimitive> gP(new GeometryPrimitive(tri, m, l));
+        p.push_back(gP);
+    }
+
+    Vector3f lightColor(8.0f * Vector3f(0.747f + 0.058f, 0.747f + 0.258f, 0.747f) + 15.6f * Vector3f(0.740f + 0.287f, 0.740f + 0.160f, 0.740f) + 18.4f * Vector3f(0.737f + 0.642f, 0.737f + 0.159f, 0.737f));
+    for (int i = 0; i < mesh6->nTriangles; i++)
+    {
+        std::shared_ptr<Triangle> tri(new Triangle(mesh6, i, P, O));
+        std::shared_ptr<Material> m = std::make_shared<MatteMaterial>(Vector3f(1, 1, 1), 20);
+        std::shared_ptr<AreaLight> l = std::make_shared<AreaLight>(tri, 1, lightColor);
+        std::shared_ptr<GeometryPrimitive> gP(new GeometryPrimitive(tri, m, l));
+        p.push_back(gP);
+        lights.push_back(l);
+    }
+}
+
+void Init(std::vector<std::shared_ptr<Primitive>> &p, std::vector<std::shared_ptr<Light>> &lights)
+{
+    int nodeCount;
+    BVHAccel BVH(p, HLBVH, 256, nodeCount);
+
+    std::shared_ptr<Filter> filter(new MitchellFilter(Vector2f(2.0f, 2.0f), 1 / 3.0f, 1 / 3.0f));
+    Film *film = new Film(Vector2i(460, 460), "image.ppm", filter);
+    std::shared_ptr<Camera> camera(new PerspectiveCamera(film, -0.1, -5000.0, 125.0 / 125.0, 38.0));
+    camera->setViewMat(Vector3f(278, 273, -800), Vector3f(278, 273, 100), Vector3f(0, 1, 0));
+    StratifiedSampler t(8, 8, 0, 1, true);
+    Scene scene(std::make_shared<BVHAccel>(BVH), lights);
+    std::shared_ptr<Integrator> integrator = std::make_shared<PathIntegrator>(camera, std::make_shared<StratifiedSampler>(t), 4);
+    integrator->Render(scene);
 }
 
 int main()
 {
-    // Image
-    Material *white_diffuse = new Material(Vector3f(1, 1, 1), DIFFUSE, Vector3f(0, 0, 0));
-    Material *red_diffuse = new Material(Vector3f(1, 0, 0), DIFFUSE, Vector3f(0, 0, 0));
-    Material *skyblue_diffuse = new Material(Vector3f(0.5, 0.7, 1), DIFFUSE, Vector3f(0, 0, 0));
+    std::vector<std::shared_ptr<Primitive>> p;
+    std::vector<std::shared_ptr<Light>> light;
 
-    Sphere sphere1(Vector3f(0.0, -0.07, -5.0), 0.7, white_diffuse);
-    Sphere shpere3(Vector3f(0.0, -100.0, -5), 99.2, white_diffuse);
-    Sphere sphere2(Vector3f(2.5, 0.0, -5.0), 0.8, red_diffuse);
-    Sphere sphere4(Vector3f(-2.0, 0.1, -3.0), 0.9, skyblue_diffuse);
-
-    std::vector<Object> objects;
-
-    objects.push_back(sphere1);
-    objects.push_back(sphere2);
-    objects.push_back(shpere3);
-    objects.push_back(sphere4);
-
-    AreaLight light(Vector3f(2, 4, -3), Vector3f(1, 1, 1), 10.0);
-
-    Camera camera(Vector3f(0, 0, 0), Vector3f(-0.2, 0, -1), Vector3f(0, 1.0, 0), 480, 260, 64);
-
-    Scene scene(camera, light, objects);
-
-    cast_ray(scene);
-
-    camera.image_color();
-
-    return 0;
+    Preprocess(p, light);
+    Init(p, light);
 }
