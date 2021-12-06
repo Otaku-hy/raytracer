871a8c030af3befa59f60995573a25e43235089e
张航宇 <zhanghangyu86@gmail.com>
2021-12-06 23:08:35 +0800

    improve robustness

--- Interaction/Interaction.cpp
+++ Interaction/Interaction.cpp
@@ -13,13 +13,23 @@ bool Interaction::IsSurfaceInteraction()
     return true;
 }
 
+/* note that when spawn ray, it's important that the offset value set either a little bit upper 
+than the surface, that is the direction is out the surface, or a little bit lower than the 
+surface, that is the direction is in the surface
+*/
+
 Ray Interaction::SpawnRay(const Interaction &p2)
 {
-    return Ray(this->pos + this->norm * EPISILON, (p2.pos - this->pos).normalized(), distance(this->pos, p2.pos) - 2 * EPISILON);
+    Vector3f direction = (p2.pos - this->pos).normalized();
+    bool out = SameHemisphere(direction, norm);
+    float originOffset = out ? EPISILON : -EPISILON;
+    return Ray(this->pos + this->norm * originOffset, direction, distance(this->pos, p2.pos) - 2 * EPISILON);
 }
 
 Ray Interaction::SpawnRay(const Vector3f &direction)
 {
-    Vector3f dir = direction;
-    return Ray(this->pos + this->norm * EPISILON, dir.normalized());
+    Vector3f dir = direction.normalized();
+    bool out = SameHemisphere(dir, norm);
+    float originOffset = out ? EPISILON : -EPISILON;
+    return Ray(this->pos + this->norm * originOffset, dir);
 }
